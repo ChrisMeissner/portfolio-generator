9cc76be912ff786c25018eac9b7081faa5d5ddc1
Chris Meissner <chrismeissner@Chriss-MBP.lan>
2020-10-07 16:24:22 -0700

    add inquirer

--- app.js
+++ app.js
@@ -8,13 +8,92 @@ const inquirer = require('inquirer');
 //  if (err) throw err;
 
 // console.log('Portfolio complete! Check out index.html to see the output!');
-//s});
+//});
+
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your project?'
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub Username'
+    },
+    {
+      type: 'input',
+      name: 'about',
+      message: 'Provide some information about yourself:'
+    },
+    {
+      type: 'input',
+      name: 'about',
+      message: 'Provide some information about yourself:'
+    },
+    {
+      type: 'input',
+      name: 'about',
+      message: 'Provide some information about yourself:'
+    }
+
+  ]);
+};
+
+const promptProject = portfolioData => {
+  console.log(`
+=================
+Add a New Project
+=================
+`);
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is the name of your project?'
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Provide a description of the project (Required)'
+    },
+    {
+      type: 'checkbox',
+      name: 'languages',
+      message: 'What did you build this project with? (Check all that apply)',
+      choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+    },
+    {
+      type: 'input',
+      name: 'link',
+      message: 'Enter the GitHub link to your project. (Required)'
+    },
+    {
+      type: 'confirm',
+      name: 'feature',
+      message: 'Would you like to feature this project?',
+      default: false
+    },
+    {
+      type: 'confirm',
+      name: 'confirmAddProject',
+      message: 'Would you like to enter another project?',
+      default: false
+    }
+  ]);
+};
+
+promptUser()
+  .then(answers => console.log(answers))
+  .then(promptProject)
+  .then(projectAnswers => console.log(projectAnswers));
 
 inquirer
   .prompt([
     {
       type: 'input',
-      name: 'name',
+      input: 'name',
       message: 'What is your name?'
     }
   ])
